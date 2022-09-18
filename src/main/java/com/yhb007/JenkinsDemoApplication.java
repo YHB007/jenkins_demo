43db6d50cadf8203ceab1a2f519e51856c82361a
yuhaibo <yu.haibo@xyb2b.com>
2022-09-18 09:58:16 +0800

    初始化提交

---
+++ src/main/java/com/yhb007/JenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yhb007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDemoApplication.class, args);
+    }
+
+}
